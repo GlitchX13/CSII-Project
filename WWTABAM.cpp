1c75f179cb6e59d43f5910bba46c4ab26a4ebe57
GlitchX13 <tyler.french13@yahoo.com>
2016-06-03 09:53:05 -0700

    Update WWTABAM.cpp

--- WWTABAM.cpp
+++ WWTABAM.cpp
@@ -1,8 +1,3 @@
-# CSII-Project
-
-#include <iostream>
-#include "MillionaireInterface.h"
-using namespace std;
 /* ***************************
 Tyler French & Riley Green
 4/13/16
@@ -11,22 +6,34 @@ This program will ask questions
 that follow a similar format
 the actual game show.
 *************************** */
+#include "MillionaireInterface.h"
+using namespace std;
 int main() {
-	//Step 1: Declare Variables
-
-	//Step 2: Introduce The User
-
+	//create variable for class functions
 	player myPlayer;
-	myPlayer.read();
-	system("cls");
-	myPlayer.playintro();
-	system("cls");
-	do {
-		myPlayer.question();
-		myPlayer.choice();
-		system("cls");
-	} while (!myPlayer.result());
-	myPlayer.leaving();
 
-	//Step 3: ....
+	do {
+		//Step 1: Get The Player's Name
+		system("cls");
+		myPlayer.read();
+
+		//Step 2: Welcome The User
+		system("cls");
+		myPlayer.playintro();
+
+		system("cls");
+		do {
+			//Step 3: Ask The Question
+			myPlayer.question();
+
+			//Step 4: Get User's Answer Selection
+			myPlayer.choice();
+			system("cls");
+		} while (!myPlayer.result());	//Step 5: Continue Asking Questions 
+	} while (myPlayer.win());	//Step 6: Player Wins
+
+	//Step 7: Thank The User & Exit Program
+	system("cls");
+	cout << endl << endl << "Thanks for playing!";
+	cin.get();
 }
