793e6e3dce34454101fcaece0b0be404eec161e0
GlitchX13 <tyler.french13@yahoo.com>
2016-06-03 09:52:45 -0700

    Update MillionaireInterface.h

--- MillionaireInterface.h
+++ MillionaireInterface.h
@@ -1,19 +1,30 @@
+/* ***************************
+The MillionaireInterface.h
+contains the public and
+private class variables and
+functions as well as mainly 
+the classes that store them
+*************************** */
 #include <iostream>
+#include <Windows.h>
+#include <MMSystem.h>
+#include <iomanip>
+#pragma comment(lib, "winmm.lib")
 #include <stdlib.h>
 using namespace std;
-/* ***************************
-*************************** */
+
 const int SIZEN = 100;
 class million {
 	public:
 		million();
 		~million();
 		void Intro(char *name, int &score);
-		void Question(char *name, int qnum, int &score);
-		void Choice(char *name, int qnum, bool fifty, bool call, bool ask, int &score, bool &right);
-		bool Result(char *name, int qnum, bool fifty, bool call, bool ask, int &score, bool &right, bool &leave);
+		void Question(char *name, int qnum, int &score,bool &fifty,bool &call,bool &ask);
+		void Choice(char *name, int qnum, bool &fifty, bool &call, bool &ask, int &score, bool &right);
+		bool Result(char *name, int qnum, bool &fifty, bool &call, bool &ask, int &score, bool &right, bool &leave);
 		void Walk(char *name, int qnum, int score);
-		void Leaving (char *name, int qnum, int &score);
+		bool Win(char *name, int qnum, int &score);
+		bool Leaving (char *name, int qnum, int &score);
 
 	private:
 		int qnum;
@@ -33,10 +44,12 @@ class player {
 		bool result();
 		void walk();
 		void table();
-		void leaving();
+		bool win();
+		bool leaving();
 	private:
 		int qnum, score;
 		bool fifty, call, ask, right, leave;
 		char *first;
 		million *millionptr;
 };
+
